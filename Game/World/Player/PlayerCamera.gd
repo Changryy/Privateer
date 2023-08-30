3a03e57a6ffe203a89b4de2a8073b85365b90781
Lindo <c@changry.no>
2023-08-30 23:07:16 +0200

    Smooth camera movement

---
+++ Game/World/Player/PlayerCamera.gd
@@ -0,0 +1,31 @@
+extends Camera2D
+class_name PlayerCamera
+
+
+@export var mouse_follow: float = .2
+@export var default_zoom: float = .5
+@export var max_zoom: float = .4
+
+var screen_shake_offset := Vector2.ZERO
+
+
+
+func _process(_delta: float) -> void:
+	var pos := get_viewport().get_mouse_position() - get_viewport_rect().size / 2.
+	
+	offset = pos * mouse_follow
+	offset += screen_shake_offset
+	
+	var d := (pos / get_viewport_rect().size).length()
+	
+	zoom = Vector2.ONE * lerpf(default_zoom, max_zoom, smoothstep(0, 1, d))
+
+
+
+
+
+
+
+
+
+
--- Game/World/Root.gd
+++ Game/World/Root.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	$Polygon2D/Ocean.play("Test")
+	$Ocean.play("Test")
 
 
 
